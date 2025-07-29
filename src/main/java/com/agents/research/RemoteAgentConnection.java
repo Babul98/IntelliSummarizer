ad7caedd114bf604ef3868c2aa46b121126bae04
Shresth72 <97455610+Shresth72@users.noreply.github.com>
2025-07-29 19:31:50 +0000

    some classes

---
+++ src/main/java/com/agents/research/RemoteAgentConnection.java
@@ -0,0 +1,7 @@
+package com.agents.research;
+
+/** A class to hold the connections to the Remote Agents. */
+public class RemoteAgentConnection {
+
+  public static void main(String[] args) {}
+}
---
+++ src/main/java/com/agents/research/ResearchAgent.java
@@ -0,0 +1,10 @@
+package com.agents.research;
+
+/**
+ * The ResearchAgent class is responsible for initiating research workflows, and is a Host Agent
+ * which can communicate to other Agents, hence extending it to perform various tasks.
+ */
+public class ResearchAgent {
+
+  public static void main(String[] args) {}
+}
